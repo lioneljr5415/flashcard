eee1356fac198c7c5e744f7e8cc832cc864341b0
lioneljr5415 <136764104+lioneljr5415@users.noreply.github.com>
2024-11-11 10:58:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Javascript',
+    'O Javascript é uma linuagem deprogramação.'
+    )
+
+    criaCartao(
+        'Programação',
+        'O que é uma função?',
+        'Uma unção é um bloco de código que executa uma tarefa.'
+    )
+
+    criaCartao(
+        'Geograia',
+        'Qual é a capital da França',
+        'A capital da França é Paris.'
+    )
